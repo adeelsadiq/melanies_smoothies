ac45608dca0378286809affbb736bed509670479
Adeel Sadiq <43704601+adeelsadiq@users.noreply.github.com>
2025-07-03 16:25:25 +0100

    fgerg

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,9 @@ cnx= st.connection("snowflake")
 session = cnx.session()
 name_on_order = st.text_input("Name on Smoothie:", "")
 st.write("the name on your smoothie will be: ", name_on_order)
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+st.dataframe(data=my_dataframe, use_container_width= True)
+st.stop()
 ingredients_list = st.multiselect(
     "What are your favourite fruits?",
     my_dataframe,
