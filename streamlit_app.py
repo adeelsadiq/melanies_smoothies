0355e61fecb2b38b4c5a82ba21084bc5957b43b2
Adeel Sadiq <43704601+adeelsadiq@users.noreply.github.com>
2025-07-03 15:58:44 +0100

    moved response to bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 import requests
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw: ")
 st.write(
@@ -41,5 +41,5 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'! ', icon="✅") 
-
+st.text(smoothiefroot_response.json())
 # st.dataframe(data=my_dataframe, use_container_width=True)
